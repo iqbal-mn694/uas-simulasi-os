ec6c4d9a3bb3e9e14fbf49e8f9d33e45b72183fe
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2025-05-02 16:55:02 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import SystemSimulator from './components/SystemSimulator.jsx'
+import App from './app.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SystemSimulator />
+    <App />
   </StrictMode>,
 )
